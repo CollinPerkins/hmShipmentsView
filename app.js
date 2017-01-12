3950d4e9bdc637dc3d179b1795ee91a88a358d9a
Collin Perkins <collinperkinsweb@gmail.com>
2017-01-12 17:02:10 -0600

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname + '/'));
-
+app.use(cors());
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 });
