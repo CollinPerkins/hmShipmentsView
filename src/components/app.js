851abd25a68aea6e463db0e1119be3529a04b0dd
Collin Perkins <collinperkinsweb@gmail.com>
2017-01-12 16:43:31 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+// import Shipment from 'quote.js';
+import Shipment from './shipment';
+import Griddle from 'griddle-react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shipments: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://gentle-escarpment-56373.herokuapp.com/allShipments`)
+    .then(res => {
+      console.log(res.data);
+      this.setState({shipments: res.data})
+    });
+  }
+
+  render() {
+    return (
+      <div className="quoteConainer">
+        <Shipment />
+        <Griddle  results={this.state.shipments} resultsPerPage={50} tableClassName="table" showFilter={true} columns={['orderNumber', 'sku', 'lot', 'qty', 'date']} showSettings={true}/>
+      </div>
+    );
+  }
+}
---
+++ src/components/shipment.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Shipment extends Component {
+  render() {
+    return (
+      <div className="centerText">
+      
+      </div>
+    );
+  }
+}
